4e9ebe4458c7a3c5af5d39798033841be0375d5d
Quicksilver0218 <38100804+Quicksilver0218@users.noreply.github.com>
2021-06-16 13:22:31 +0800

    Update deparam.js

--- url/deparam.js
+++ url/deparam.js
@@ -33,65 +33,55 @@ function deparam(params, coerce = false) {
             keys = keys.shift().split('[').concat(keys);
             
             keys_last = keys.length - 1;
-        } else {
+        } else
             // Basic 'foo' style key.
             keys_last = 0;
-        }
         
         // Are we dealing with a name=value pair, or just a name?
         if (param.length === 2) {
             val = decodeURIComponent(param[1]);
           
-        // Coerce values.
-        if (coerce) {
-            val = val && !isNaN(val)              ? +val              // number
-                : val === 'undefined'             ? undefined         // undefined
-                : coerce_types[val] !== undefined ? coerce_types[val] // true, false, null
-                : val;                                                // string
-        }
-          
-        if ( keys_last ) {
-            // Complex key, build deep object structure based on a few rules:
-            // * The 'cur' pointer starts at the object top-level.
-            // * [] = array push (n is set to array length), [n] = array if n is 
-            //   numeric, otherwise object.
-            // * If at the last keys part, set the value.
-            // * For each keys part, if the current level is undefined create an
-            //   object or array based on the type of the next keys part.
-            // * Move the 'cur' pointer to the next level.
-            // * Rinse & repeat.
-            for (; i <= keys_last; i++) {
-                key = keys[i] === '' ? cur.length : keys[i];
-                cur = cur[key] = i < keys_last
-                    ? cur[key] || (keys[i+1] && isNaN(keys[i+1]) ? {} : [])
-                    : val;
+            // Coerce values.
+            if (coerce) {
+                val = val && !isNaN(val)              ? +val              // number
+                    : val === 'undefined'             ? undefined         // undefined
+                    : coerce_types[val] !== undefined ? coerce_types[val] // true, false, null
+                    : val;                                                // string
             }
             
-        } else {
-            // Simple key, even simpler rules, since only scalars and shallow
-            // arrays are allowed.
-                
-            if (Array.isArray(obj[key])) {
-                // val is already an array, so push on the next value.
-                obj[key].push( val );
-                
-            } else if (obj[key] !== undefined) {
-                // val isn't an array, but since a second value has been specified,
-                // convert val into an array.
-                obj[key] = [obj[key], val];
-                
+            if ( keys_last ) {
+                // Complex key, build deep object structure based on a few rules:
+                // * The 'cur' pointer starts at the object top-level.
+                // * [] = array push (n is set to array length), [n] = array if n is 
+                //   numeric, otherwise object.
+                // * If at the last keys part, set the value.
+                // * For each keys part, if the current level is undefined create an
+                //   object or array based on the type of the next keys part.
+                // * Move the 'cur' pointer to the next level.
+                // * Rinse & repeat.
+                for (; i <= keys_last; i++) {
+                    key = keys[i] === '' ? cur.length : keys[i];
+                    cur = cur[key] = i < keys_last
+                        ? cur[key] || (keys[i+1] && isNaN(keys[i+1]) ? {} : [])
+                        : val;
+                }
             } else {
-                // val is a scalar.
-                obj[key] = val;
+                // Simple key, even simpler rules, since only scalars and shallow
+                // arrays are allowed.
+                if (Array.isArray(obj[key]))
+                    // val is already an array, so push on the next value.
+                    obj[key].push( val );
+                else if (obj[key] !== undefined)
+                    // val isn't an array, but since a second value has been specified,
+                    // convert val into an array.
+                    obj[key] = [obj[key], val];
+                else
+                    // val is a scalar.
+                    obj[key] = val;
             }
-        }
-          
-        } else if (key) {
+        } else if (key)
             // No value was defined, so set something meaningful.
-            obj[key] = coerce
-            ? undefined
-            : '';
-        }
+            obj[key] = coerce ? undefined : '';
     });
       
     return obj;
