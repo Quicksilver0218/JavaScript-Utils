0704f012953e1b0feca739532c33eeb991a3af97
Quicksilver0218 <38100804+Quicksilver0218@users.noreply.github.com>
2021-07-21 11:50:26 +0800

    Update replacePlaceholder.js

--- string/replacePlaceholder.js
+++ string/replacePlaceholder.js
@@ -1,22 +1,23 @@
 /**
  * Replace the placeholders with the given parameters in a string.
  * e.g. replacePlaceholder('$0$$$1$$2', ['a', 'b', 'c'], '$') -> 'a$b$2'
+ * e.g. replacePlaceholder('$i$$$j$$k', {i: 'a', j: 'b', k: 'c'}, '$') -> 'a$b$k'
  * @param {string} str The string with placeholders to be replaced.
- * @param {string[]} params The parameters to replace the placeholders.
+ * @param {string[]} map The map used to replace the placeholders.
  * @param {string} marker The marker used to mark the placeholders. 2 continuous markers is the escape sequence which represents keeping 1 original marker.
  * @returns {string} The result string that the placeholders are replaced.
  */
-function replacePlaceholder(str, params, marker = '$') {
+function replacePlaceholder(str, map, marker = '$') {
     const parts = str.split(marker + marker);
     parts.forEach((part, i) => {
-        parts[i] = replacePlaceholderRecurse(part, params, marker, 0);
+        parts[i] = replacePlaceholderRecurse(part, map, marker, 0);
     });
     return parts.join(marker);
 }
 
-function replacePlaceholderRecurse(str, params, marker, level) {
-    const parts = str.split(marker + level);
-    if (level + 1 < params.length)
-        parts.forEach((part, i) => parts[i] = replacePlaceholderRecurse(part, params, marker, level + 1));
-    return parts.join(params[level]);
+function replacePlaceholderRecurse(str, map, marker, level) {
+    const parts = str.split(marker + Object.keys(map)[level]);
+    if (level + 1 < Object.keys(map).length)
+        parts.forEach((part, i) => parts[i] = replacePlaceholderRecurse(part, map, marker, level + 1));
+    return parts.join(map[Object.keys(map)[level]]);
 }
